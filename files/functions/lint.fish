f1cd62228ed6efae9addaacb96bfde8dac3c02e5
Jared Cuffe <jcuffe@gmail.com>
2022-12-23 14:13:32 -0800

    Improves lint function.

--- files/functions/lint.fish
+++ files/functions/lint.fish
@@ -1,5 +1,8 @@
-# Lints only files that differ from origin/main in the current branch
 function lint
+    # Fetch latest code from origin/main
+    git fetch origin main:main > /dev/null
+
+    # Lint only files that differ from origin/main
     npx eslint (git diff --name-only origin/main | grep -E '.*\.(js|ts)$')
 end
 
