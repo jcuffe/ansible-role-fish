c3908fa30068e870dad079f6b3f92e651d40958d
Jared Cuffe <jcuffe@gmail.com>
2023-09-08 16:54:40 -0700

    Updates environment.

--- files/conf.d/init.fish
+++ files/conf.d/init.fish
@@ -4,3 +4,13 @@
 if functions --query nvm
     nvm use --silent
 end
+
+# If go is installed, add it to the path
+if test -d /usr/local/go
+    set --prepend PATH /usr/local/go/bin
+end
+
+# If kakoune is installed, set it as the editor
+if command --query kak
+    set EDITOR kak
+end
