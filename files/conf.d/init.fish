3d1705893f4ce44cd7d3784367348c2227e672ee
Jared Cuffe <jcuffe@gmail.com>
2023-09-08 17:43:31 -0700

    Removes redundant function call.

--- files/conf.d/init.fish
+++ files/conf.d/init.fish
@@ -1,10 +1,5 @@
 # Run some commands as soon as the shell is started
 
-# If nvm is available, load the appropriate version of node
-if functions --query nvm
-    nvm use --silent
-end
-
 # If go is installed, add it to the path
 if test -d /usr/local/go
     set --prepend PATH /usr/local/go/bin
