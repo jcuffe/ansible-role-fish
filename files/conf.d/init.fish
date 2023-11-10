16f61cde6f92240dc20810daaa808a685bf7595e
Jared Cuffe <jcuffe@gmail.com>
2023-11-10 09:08:41 -0800

    Adds asdf init.

--- files/conf.d/init.fish
+++ files/conf.d/init.fish
@@ -10,6 +10,11 @@ if test -d /usr/local/zig
     set --prepend PATH /usr/local/zig
 end
 
+# If asdf is present, load its commands
+if test -d ~/.asdf
+    source ~/.asdf/asdf.fish
+end
+
 # If kakoune is installed, set it as the editor
 if command --query kak
     set EDITOR kak
